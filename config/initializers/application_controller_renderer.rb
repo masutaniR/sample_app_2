19189c2e2a1f6c774718a1066061cf16b87dc797
masutaniR <popohu87@gmail.com>
2020-12-26 02:15:39 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '12f7b6862b671a964cece22afbb111d3c7c86be02e6fab650a308511eee2cbad2e4ce5d53b71e4575d5116721e2550ae8a510ad559b8abdbfec946baccc9bbda'
